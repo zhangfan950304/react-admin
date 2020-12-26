7c401d98b61598d52893779454c563761bc21355
张凡 <zhangfan@zhangfandeMacBook-Pro.local>
2020-12-26 19:28:46 +0800

    文件创建+路由

---
+++ src/pages/admin/admin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//后台管理的路由组建
+export default class Admin extends React.Component{
+    render(){
+        return(
+            <div>
+                admin
+            </div>
+        )
+    }
+}
---
+++ src/pages/login/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './login.sass';
+
+//登陆的路由组建
+export default class Login extends React.Component{
+    render(){
+        return(
+            <div className="boy">
+                login
+            </div>
+        )
+    }
+}
